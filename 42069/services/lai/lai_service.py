0db510a65cac68c4612d8f24dcf62742cb513529
passawityakul <p.yakul@icloud.com>
2021-11-22 21:59:19 +0700

    update docker compose and config file

--- 42069/services/lai/lai_service.py
+++ 42069/services/lai/lai_service.py
@@ -1,12 +1,18 @@
 from typing import Union
 
-from utils.util_func import load_conf
+from utils.util_func import load_conf, load_env, env
 from services.lai.lai_util import LaiUtil
 
 
 class LaiService:
     def __init__(self):
-        self.conf: dict[str, Union[str, int]] = load_conf(dir='./42069/configs/', name='linenoti.json')
+        self.conf = {}
+        try:
+            self.conf: dict[str, Union[str, int]] = load_conf(dir='./42069/configs/', name='linenoti.json')
+        except Exception:
+            load_env('./configs/', name='lai')
+            self.conf['url'] = env('URL')
+            self.conf['token'] = env('TOKEN')
         self.util: LaiUtil = LaiUtil(self.conf)
 
     def send_msg(self, msg: str) -> dict[str, Union[str, int]]:
--- 42069/services/twither/twither_service.py
+++ 42069/services/twither/twither_service.py
@@ -6,7 +6,7 @@ from services.twither.twither_util import TwitherUtil
 
 class TwitherService:
     def __init__(self):
-        self.conf: dict[str, Union[str, int]] = load_conf(dir='configs/', name='linenoti.json')
+        self.conf: dict[str, Union[str, int]] = load_conf(dir='configs/', name='linenot.json')
         self.util: TwitherUtil = TwitherUtil(self.conf)
 
     def do_something(self, something: str) -> dict[str, Union[str, int]]:
