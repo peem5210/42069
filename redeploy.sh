1b49adfab88f7aa56bebb518bf28b2c913dc7d88
passawityakul <p.yakul@icloud.com>
2021-11-29 12:24:17 +0700

    Update script [noBUILD][noCD]

--- redeploy.sh
+++ redeploy.sh
@@ -3,3 +3,4 @@ docker-compose -f ./.github/scripts/docker-compose-ci.yml pull
 docker-compose -f ./.github/scripts/docker-compose-ci.yml down
 docker network create 42069-backend-net --subnet 172.24.24.0/24
 docker-compose -f ./.github/scripts/docker-compose-ci.yml up --build -d
+docker image prune -f
